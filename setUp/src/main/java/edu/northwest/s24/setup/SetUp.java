2977b59035f0547d239f763b8adae60f58e8486b
5r1kanth <S560174@nwmissouri.edu>
2024-01-15 21:38:32 -0600

    Commit after changing the description

--- setUp/src/main/java/edu/northwest/s24/setup/SetUp.java
+++ setUp/src/main/java/edu/northwest/s24/setup/SetUp.java
@@ -1,12 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 package edu.northwest.s24.setup;
 
 /**
- *
- * @author S560174
+ * class: 44542-01 object-oriented programming
+ * 
+ * @author Srikanth Pamulapati
+ * Description: Making sure everything works
+ * Due: MM/DD/YYYY
+ * I pledge that i have completed the programming assignment independently.
+ * I have not copied the code from a student, internet, or any other source.
+ * I will not share my source code with anyone under any circumstances.
  */
 public class SetUp {
 
