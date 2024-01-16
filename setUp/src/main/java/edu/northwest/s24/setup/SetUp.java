793cdc49c1d2628229c5ed70134291976a23a569
5r1kanth <S560174@nwmissouri.edu>
2024-01-15 21:08:56 -0600

    First Commit during setUp!

---
+++ setUp/src/main/java/edu/northwest/s24/setup/SetUp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.northwest.s24.setup;
+
+/**
+ *
+ * @author S560174
+ */
+public class SetUp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! in setUp");
+    }
+}
